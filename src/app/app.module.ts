9f87f95e750738c1f5dfb4e006c7610d6ce193fb
Alvaro Izquierdo <aizquierdo@salutic.es>
2018-09-08 07:49:01 +0200

    import httpclient module y app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -19,6 +21,7 @@ import { SpeakersProvider } from '../providers/speakers/speakers';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule,
     NgxModelModule
   ],
   bootstrap: [IonicApp],
